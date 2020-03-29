ece3209aed4aae8542bbd205877eaae3624ed3bc
Christopher Jalen Tan <tanchris20@gmail.com>
2020-03-29 22:36:29 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -1,5 +1,11 @@
+var basicTeenager
 function basicTeenager(age) {
-
+  if (age >=13 && <=19) {
+    console.log('You are a teenager!');
+  }
+  else (age <=12 && >=20) {
+    console.log('You are not a teenager!');
+  }
 }
 
 function teenager(age) {
